5182fc9b067d0968fcccda0d25739b5c7e82f927
gjcochran <garrettjcochran@gmail.com>
2024-08-13 18:09:20 -0600

    feat: updated Benchmark table CSS

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -25,4 +25,9 @@ export default defineConfig({
       { icon: "github", link: "https://github.com/vuejs/vitepress" },
     ],
   },
+  vite: {
+    ssr: {
+      noExternal: ["prismjs"],
+    },
+  },
 });
