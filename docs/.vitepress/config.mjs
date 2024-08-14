e5d75b40036b4b4abb6714588b3c27d935bf4e04
jamesdrabinsky <james.drabinsky@gmail.com>
2024-08-13 17:07:59 -0700

    feat: made nav visible on homepage

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -6,22 +6,19 @@ export default defineConfig({
   cleanUrls: true,
   themeConfig: {
     nav: [{ text: "Home", link: "/" }],
-
-    sidebar: [
-      { text: "Introduction", link: "/introduction" },
-      {
-        text: "Helios Architecture",
-        link: "/helios-architecture",
-      },
-      { text: "Building Helios: Design Choices", link: "/building-helios" },
-      { text: "Improving Core Platform", link: "/improving-core-platform" },
-      { text: "Scaling", link: "/scaling" },
-      { text: "Load Testing", link: "/load-testing" },
-      { text: "Automating Deployment", link: "/automating-deployment" },
-      { text: "Future Work", link: "/future-work" },
-      { text: "References", link: "/references" },
-    ],
-
+    sidebar: {
+      "/": [
+        { text: "Introduction", link: "/introduction" },
+        { text: "Helios Architecture", link: "/helios-architecture" },
+        { text: "Building Helios: Design Choices", link: "/building-helios" },
+        { text: "Improving Core Platform", link: "/improving-core-platform" },
+        { text: "Scaling", link: "/scaling" },
+        { text: "Load Testing", link: "/load-testing" },
+        { text: "Automating Deployment", link: "/automating-deployment" },
+        { text: "Future Work", link: "/future-work" },
+        { text: "References", link: "/references" },
+      ],
+    },
     socialLinks: [
       { icon: "github", link: "https://github.com/vuejs/vitepress" },
     ],
